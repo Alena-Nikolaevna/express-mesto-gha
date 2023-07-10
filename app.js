a184aceb799044e9b84e1ff2ddd47f9605cb27fe
Alena Nikolaevna <magnittyt@mail.ru>
2023-07-10 19:50:33 +0700

    Исправления ПР-13

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
 const app = express();
+
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
 const { ERROR_NOT_FOUND, MESSAGE_ERROR_NOT_FOUND } = require('./utils/error');
+
+// чтобы использовать его на сервере только для API,
+// где ограничитель скорости должен применяться ко всем запросам
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // // Ограничение каждого IP до 100 запросов на «окно» (здесь за 15 минут)
+  standardHeaders: true, // // Ограничение скорости возврата информация в заголовках `RateLimit-*`
+  legacyHeaders: false, // Отключить заголовки `X-RateLimit-*` (headers)
+});
+
+// для защиты приложения от некоторых широко известных веб-уязвимостей
+app.use(helmet());
+
+// для ограничения кол-ва запросов, для защиты от DoS-атак
+app.use(limiter);
 
 // mongoose.connect('mongodb://localhost:27017/mestodb');
 // подключаемся к серверу mongo
