bae935bb974c9f0ef57524f4ecfab0b39abe2d2c
Alena Nikolaevna <magnittyt@mail.ru>
2023-07-27 16:52:25 +0700

    ПР-14

--- middlewares/error.js
+++ middlewares/error.js
@@ -2,7 +2,8 @@ const ServerError = require('../errors/ServerError'); // 500 ошибка
 const ConflictError = require('../errors/ConflictError'); // 409
 
 const errorMiddlewares = (err, req, res, next) => {
-  const { statusCode = ServerError, message } = err;
+  const { statusCode = ServerError } = err;
+  const message = statusCode === ServerError ? 'На сервере произошла ошибка.' : err.message;
 
   if (err.code === 11000) {
     res.status(ConflictError).send({ message: 'Пользователь с указанным email уже существует' });
